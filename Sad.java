e51ba48687ff71466b9bb98c76de81e592d9948c
charl <charl@164.76.17.193>
2025-02-10 12:31:02 -0500

    I'm sad.

---
+++ Sad.java
@@ -0,0 +1,6 @@
+
+public class Sad {
+	public static void sad() {
+		System.out.println("We are all sad.");
+	}
+}
--- driver.java
+++ driver.java
@@ -3,5 +3,7 @@ public class driver {
 
 	public static void main(String[] args) {
 		System.out.println("Our team is getting started.");
+		
+		Sad.sad();
 	}
 }
